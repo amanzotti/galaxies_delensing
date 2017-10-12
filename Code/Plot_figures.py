90d95d0fba82569dd621f88b8716ab6196c44921
Alessandro Manzotti <manzotti.alessandro@gmail.com>
2017-10-12 09:42:04 -0500

    new figures same colors

--- Code/Plot_figures.py
+++ Code/Plot_figures.py
@@ -7,9 +7,9 @@ import configparser as ConfigParser
 import camb
 from camb import initialpower
 try:
-    import functools32
+  import functools32
 except ImportError:
-    import functools as functools32
+  import functools as functools32
 
 
 # ============================================
@@ -43,7 +43,7 @@ plt.rcParams['font.size'] = font_size
 plt.rcParams['axes.labelsize'] = font_size * 1.3
 plt.rcParams['axes.linewidth'] = font_size / 10.
 plt.rcParams['axes.titlesize'] = font_size * 1.3
-plt.rcParams['legend.fontsize'] = font_size/ 1.2
+plt.rcParams['legend.fontsize'] = font_size / 1.2
 plt.rcParams['xtick.labelsize'] = font_size / 1.2
 plt.rcParams['ytick.labelsize'] = font_size / 1.2
 plt.rcParams['xtick.major.width'] = font_size / 10.
@@ -80,22 +80,24 @@ plt.rcParams['legend.handlelength'] = font_size / 9.
 # =============================================
 
 BB_contr = np.genfromtxt('../Data/BB_contribution.csv', delimiter=',')
-
+# colormap = plt.cm.Set1
+colormap = plt.cm.Vega10
 # dictionary of colors
 colors = {}
-colors['CIB'] = plt.cm.Set1(0)
-colors['DES'] = plt.cm.Set1(1)
-colors['WISE'] = plt.cm.Set1(2)
-colors['DESI'] = plt.cm.Set1(3)
-colors['LSST'] = plt.cm.Set1(4)
-colors['SKA'] = plt.cm.Set1(5)
+colors['CIB'] = colormap(0)
+colors['DES'] = colormap(1)
+colors['WISE'] = colormap(2)
+colors['DESI'] = colormap(3)
+colors['LSST'] = colormap(4)
+colors['SKA'] = colormap(5)
 
 
 plt.rcParams["figure.figsize"] = fig_dims
 
 labels = ['wise', 'cib', 'des_bin0', 'des_bin1', 'des_bin2', 'des_bin3']
 cmb = 'Planck'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 
 labels = ['des_bin0', 'des_bin1', 'des_bin2', 'des_bin3']
@@ -106,12 +108,12 @@ lbins, _, _, rho_gals_des, _ = multiple_survey_delens.main(labels, cmb)
 fg = plt.figure(figsize=fig_dims)
 ax1 = fg.add_subplot(111)
 
-plt.plot(lbins, rho['cib'], color = colors['CIB'], label='CIB')
-plt.plot(lbins, rho['wise'], color = colors['WISE'],label='WISE')
-plt.plot(lbins, rho_gals_des,color = colors['DES'], label='DES')
-plt.plot(lbins, rho_cmb, label='Planck', color = plt.cm.Set1(6))
-plt.plot(lbins, rho_gals, label='DES+CIB+WISE (LSS S2)', color = plt.cm.Set1(7))
-plt.plot(lbins, rho_comb, label='LSS S2 + Planck', color = plt.cm.Set1(8))
+plt.plot(lbins, rho['cib'], color=colors['CIB'], label='CIB')
+plt.plot(lbins, rho['wise'], color=colors['WISE'], label='WISE')
+plt.plot(lbins, rho_gals_des, color=colors['DES'], label='DES')
+plt.plot(lbins, rho_cmb, label='Planck', color=colormap(6))
+plt.plot(lbins, rho_gals, label='DES+CIB+WISE (LSS S2)', color=colormap(7))
+plt.plot(lbins, rho_comb, label='LSS S2 + Planck', color=colormap(8))
 plt.plot(BB_contr[:, 0], BB_contr[:, 1] - np.min(BB_contr[:, 1]),
          '--', alpha=0.6, linewidth=font_size / 14., color='k')
 
@@ -124,7 +126,8 @@ plt.xlim(10, 1400)
 plt.ylim(0, 1.15)
 fg.tight_layout()
 
-plt.savefig('../images/actual_scenario_planck.pdf', dpi=600, papertype='Letter')
+plt.savefig('../images/actual_scenario_planck.pdf',
+            dpi=600, papertype='Letter')
 plt.savefig('../images/actual_scenario_planck.png')
 
 
@@ -134,7 +137,8 @@ plt.savefig('../images/actual_scenario_planck.png')
 
 labels = ['wise', 'cib', 'des_bin0', 'des_bin1', 'des_bin2', 'des_bin3']
 cmb = 'now'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 
 labels = ['des_bin0', 'des_bin1', 'des_bin2', 'des_bin3']
@@ -147,12 +151,12 @@ plt.close()
 
 fg = plt.figure(figsize=fig_dims)
 
-plt.plot(lbins, rho['cib'], color = colors['CIB'],label='CIB')
-plt.plot(lbins, rho_gals_des, color = colors['DES'],label='DES')
-plt.plot(lbins, rho['wise'], color = colors['WISE'],label='WISE')
-plt.plot(lbins, rho_cmb, label='SPT Pol', color = plt.cm.Set1(6))
-plt.plot(lbins, rho_gals, label='DES+CIB', color = plt.cm.Set1(7))
-plt.plot(lbins, rho_comb, label='LSS S2 +  SPTPol', color = plt.cm.Set1(8))
+plt.plot(lbins, rho['cib'], color=colors['CIB'], label='CIB')
+plt.plot(lbins, rho_gals_des, color=colors['DES'], label='DES')
+plt.plot(lbins, rho['wise'], color=colors['WISE'], label='WISE')
+plt.plot(lbins, rho_cmb, label='SPT Pol', color=colormap(6))
+plt.plot(lbins, rho_gals, label='DES+CIB', color=colormap(7))
+plt.plot(lbins, rho_comb, label='LSS S2 +  SPTPol', color=colormap(8))
 # plt.plot(BB_contr[:, 0], BB_contr[:, 1] - np.min(BB_contr[:, 1]), '--', alpha=0.6, color='k')
 plt.legend(loc=0, ncol=2)
 plt.ylim(0, 1.2)
@@ -171,7 +175,8 @@ plt.savefig('../images/actual_scenario.png')
 labels = ['wise', 'cib', 'des_bin0', 'des_bin1', 'des_bin2',
           'des_bin3', 'desi_bin0', 'desi_bin1', 'desi_bin2', 'desi_bin3']
 cmb = 'S3'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 labels = ['desi_bin0', 'desi_bin1', 'desi_bin2', 'desi_bin3']
 cmb = 'S3'
@@ -181,10 +186,10 @@ plt.close()
 
 fg = plt.figure(figsize=fig_dims)
 
-plt.plot(lbins, rho_gals_desi, color = colors['DESI'],label='DESI')
-plt.plot(lbins, rho_cmb, label='SPT 3G', color = plt.cm.Set1(6))
-plt.plot(lbins, rho_gals, label='LSS-S2+DESI (LSS-S3)', color = plt.cm.Set1(7))
-plt.plot(lbins, rho_comb, label='LSS-S3+SPT 3G', color = plt.cm.Set1(8))
+plt.plot(lbins, rho_gals_desi, color=colors['DESI'], label='DESI')
+plt.plot(lbins, rho_cmb, label='SPT 3G', color=colormap(6))
+plt.plot(lbins, rho_gals, label='LSS-S2+DESI (LSS-S3)', color=colormap(7))
+plt.plot(lbins, rho_comb, label='LSS-S3+SPT 3G', color=colormap(8))
 # plt.plot(BB_contr[:, 0], BB_contr[:, 1] - np.min(BB_contr[:, 1]), '--', alpha=0.6, color='k')
 
 plt.legend(loc=0, ncol=2)
@@ -207,33 +212,38 @@ plt.savefig('../images/S3_scenario.png')
 labels = ['wise', 'ska10', 'cib', 'des_bin0', 'des_bin1', 'des_bin2', 'des_bin3', 'lsst_bin0', 'lsst_bin1', 'lsst_bin2',
           'lsst_bin3', 'lsst_bin4', 'lsst_bin5', 'lsst_bin6', 'lsst_bin7', 'lsst_bin8', 'lsst_bin9', 'desi_bin0', 'desi_bin1', 'desi_bin2', 'desi_bin3']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 plt.close()
 fg = plt.figure(figsize=fig_dims)
-plt.plot(lbins, rho_cmb, label='CMB S4', color = plt.cm.Set1(6))
+plt.plot(lbins, rho_cmb, label='CMB S4', color=colormap(6))
 # plt.plot(lbins, rho_gals, label='Galaxies')
-plt.plot(lbins, rho_comb, label=r'LSS-S4 + SKA' +'\n' + '+ CMB S4', color = plt.cm.Set1(8))
+plt.plot(lbins, rho_comb, label=r'LSS-S4 + SKA' +
+         '\n' + '+ CMB S4', color=colormap(8))
 # plt.plot(lbins, rho['euclid'], label='Euclid')
 labels = ['lsst_bin0', 'lsst_bin1', 'lsst_bin2',
           'lsst_bin3', 'lsst_bin4', 'lsst_bin5', 'lsst_bin6', 'lsst_bin7', 'lsst_bin8', 'lsst_bin9']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
-plt.plot(lbins, rho_gals, color = colors['LSST'],label='LSST')
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
+plt.plot(lbins, rho_gals, color=colors['LSST'], label='LSST')
 # plt.plot(BB_contr[:, 0], BB_contr[:, 1] - np.min(BB_contr[:, 1]),
 #          '--', alpha=0.6, linewidth=font_size / 14., color='k')
 
 labels = ['ska10']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
-plt.plot(lbins, rho_gals, color = colors['SKA'],label=r'SKA $10\mu$Jy')
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
+plt.plot(lbins, rho_gals, color=colors['SKA'], label=r'SKA $10\mu$Jy')
 
 
 labels = ['wise', 'cib', 'des_bin0', 'des_bin1', 'des_bin2', 'des_bin3', 'lsst_bin0', 'lsst_bin1', 'lsst_bin2',
           'lsst_bin3', 'lsst_bin4', 'lsst_bin5', 'lsst_bin6', 'lsst_bin7', 'lsst_bin8', 'lsst_bin9', 'desi_bin0', 'desi_bin1', 'desi_bin2', 'desi_bin3']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
-plt.plot(lbins, rho_gals, label='LSS-S3+LSST(LSS-S4)',color = plt.cm.Set1(7))
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
+plt.plot(lbins, rho_gals, label='LSS-S3+LSST(LSS-S4)', color=colormap(7))
 
 plt.legend(loc=0, ncol=2)
 plt.ylim(0.45, 1.25)
@@ -253,21 +263,23 @@ plt.savefig('../images/S4_scenario_ska10.png')
 labels = ['wise', 'ska01', 'cib', 'des_bin0', 'des_bin1', 'des_bin2', 'des_bin3', 'lsst_bin0', 'lsst_bin1', 'lsst_bin2',
           'lsst_bin3', 'lsst_bin4', 'lsst_bin5', 'lsst_bin6', 'lsst_bin7', 'lsst_bin8', 'lsst_bin9', 'desi_bin0', 'desi_bin1', 'desi_bin2', 'desi_bin3']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 plt.close()
 fg = plt.figure(figsize=fig_dims)
 # plt.plot(lbins, rho_gals, label='Galaxies')
-plt.plot(lbins, rho_comb, label='LSS-S4 + CMB S4', color = plt.cm.Set1(8))
+plt.plot(lbins, rho_comb, label='LSS-S4 + CMB S4', color=colormap(8))
 
 # plt.plot(BB_contr[:, 0], BB_contr[:, 1] - np.min(BB_contr[:, 1]),
 #          '--', alpha=0.6, linewidth=font_size / 14., color='k')
 
 labels = ['ska01']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
-plt.plot(lbins, rho_gals, color = colors['SKA'],label=r'SKA $0.1\mu$Jy')
-plt.plot(lbins, rho_cmb, label='CMB S4', color = plt.cm.Set1(6))
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
+plt.plot(lbins, rho_gals, color=colors['SKA'], label=r'SKA $0.1\mu$Jy')
+plt.plot(lbins, rho_cmb, label='CMB S4', color=colormap(6))
 
 plt.legend(loc=0, ncol=2)
 plt.ylim(0.5, 1.2)
@@ -288,19 +300,23 @@ plt.savefig('../images/S4_scenario_ska01.png')
 labels = ['lsst_bin0', 'lsst_bin1', 'lsst_bin2', 'lsst_bin3', 'lsst_bin4',
           'lsst_bin5', 'lsst_bin6', 'lsst_bin7', 'lsst_bin8', 'lsst_bin9']
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals_lsst, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals_lsst, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 labels = ['lsst']
 cmb = 'S4'
-lbins, rho_lsst, rho_comb_lsst, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho_lsst, rho_comb_lsst, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 labels = ['des_bin0', 'des_bin1', 'des_bin2', 'des_bin3']
 cmb = 'Planck'
-lbins, rho, rho_comb, rho_gals_des, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals_des, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 labels = ['des']
 cmb = 'Planck'
-lbins, rho_des, rho_comb_des, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho_des, rho_comb_des, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 labels = ['desi_bin0', 'desi_bin1', 'desi_bin2', 'desi_bin3']
 cmb = 'Planck'
@@ -313,11 +329,11 @@ lbins, rho_desi, _, _, _ = multiple_survey_delens.main(labels, cmb)
 
 plt.close()
 fg = plt.figure(figsize=fig_dims)
-plt.plot(lbins, rho_lsst['lsst'], label='LSST', color = colors['LSST'])
-plt.plot(lbins, rho_gals_lsst, linestyle='-.', color=colors['DESI'])
-plt.plot(lbins, rho_des['des'], label='DES', color = colors['DES'],)
-plt.plot(lbins, rho_gals_des, linestyle='-.', color=colors['DESI'])
-plt.plot(lbins, rho_desi['desi'], label='DESI', color = colors['DESI'])
+plt.plot(lbins, rho_lsst['lsst'], label='LSST', color=colors['LSST'])
+plt.plot(lbins, rho_gals_lsst, linestyle='-.', color=colors['LSST'])
+plt.plot(lbins, rho_des['des'], label='DES', color=colors['DES'],)
+plt.plot(lbins, rho_gals_des, linestyle='-.', color=colors['DES'])
+plt.plot(lbins, rho_desi['desi'], label='DESI', color=colors['DESI'])
 plt.plot(lbins, rho_gals_desi, linestyle='-.', color=colors['DESI'])
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$\rho$')
@@ -339,21 +355,25 @@ plt.close()
 fg = plt.figure(figsize=fig_dims)
 labels = ['cib']
 cmb = 'Planck'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 plt.plot(lbins, rho_cmb, label='Planck')
 # plt.plot(BB_contr[:, 0], BB_contr[:, 1] - np.min(BB_contr[:, 1]),
 #          '--', alpha=0.6, linewidth=font_size / 14., color='k')
 
 cmb = 'now'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 plt.plot(lbins, rho_cmb, label='Current stage')
 
 cmb = 'S3'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 plt.plot(lbins, rho_cmb, label='CMB S3')
 
 cmb = 'S4'
-lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(labels, cmb)
+lbins, rho, rho_comb, rho_gals, rho_cmb = multiple_survey_delens.main(
+    labels, cmb)
 
 
 plt.plot(lbins, rho_cmb, label='CMB S4')
@@ -369,7 +389,7 @@ fg.tight_layout()
 
 plt.savefig('../images/cmb_internal.pdf', dpi=600, papertype='Letter')
 plt.savefig('../images/cmb_internal.png')
-
+sys.exit()
 ############################################
 ############################################
 #  THIS WILL PLOT RESIDUALS FIGURES!!!!!
@@ -380,25 +400,25 @@ plt.savefig('../images/cmb_internal.png')
 
 
 def bl(fwhm_arcmin, lmax):
-    """ returns the map-level transfer function for a symmetric Gaussian beam.
-         * fwhm_arcmin      - beam full-width-at-half-maximum (fwhm) in arcmin.
-         * lmax             - maximum multipole.
-    """
-    import numpy as np
-    ls = np.arange(0, lmax + 1)
-    return np.exp(-(fwhm_arcmin * np.pi / 180. / 60.)**2 /
-                  (16. * np.log(2.)) * ls * (ls + 1.))
+  """ returns the map-level transfer function for a symmetric Gaussian beam.
+       * fwhm_arcmin      - beam full-width-at-half-maximum (fwhm) in arcmin.
+       * lmax             - maximum multipole.
+  """
+  import numpy as np
+  ls = np.arange(0, lmax + 1)
+  return np.exp(-(fwhm_arcmin * np.pi / 180. / 60.)**2 /
+                (16. * np.log(2.)) * ls * (ls + 1.))
 
 
 def nl(noise_uK_arcmin, fwhm_arcmin, lmax):
-    """ returns the beam-deconvolved noise power spectrum in units of uK^2 for
-          * noise_uK_arcmin - map noise level in uK.arcmin
-          * fwhm_arcmin     - beam full-width-at-half-maximum (fwhm) in arcmin.
-          * lmax            - maximum multipole.
-    """
-    import numpy as np
+  """ returns the beam-deconvolved noise power spectrum in units of uK^2 for
+        * noise_uK_arcmin - map noise level in uK.arcmin
+        * fwhm_arcmin     - beam full-width-at-half-maximum (fwhm) in arcmin.
+        * lmax            - maximum multipole.
+  """
+  import numpy as np
 
-    return (noise_uK_arcmin * np.pi / 180. / 60.)**2 / bl(fwhm_arcmin, lmax)**2
+  return (noise_uK_arcmin * np.pi / 180. / 60.)**2 / bl(fwhm_arcmin, lmax)**2
 
 # In[21]:
 
@@ -428,22 +448,22 @@ tens_cl = results.get_tensor_cls(3000)
 
 @functools32.lru_cache(maxsize=64)
 def clbb(r=0.1, nt=None, lmax=3000):
-    inflation_params = initialpower.InitialPowerParams()
-    if nt is None:
-        nt = -r / 8.
-    inflation_params.set_params(As=2.1e-9, r=r, nt=nt)
-    results.power_spectra_from_transfer(inflation_params)
-    return results.get_total_cls(lmax)[:, 2] * 7.42835025e12
+  inflation_params = initialpower.InitialPowerParams()
+  if nt is None:
+    nt = -r / 8.
+  inflation_params.set_params(As=2.1e-9, r=r, nt=nt)
+  results.power_spectra_from_transfer(inflation_params)
+  return results.get_total_cls(lmax)[:, 2] * 7.42835025e12
 
 
 @functools32.lru_cache(maxsize=64)
 def clbb_tens(r=0.1, nt=None, lmax=3000):
-    inflation_params = initialpower.InitialPowerParams()
-    if nt is None:
-        nt = -r / 8.
-    inflation_params.set_params(As=2.1e-9, r=r, nt=nt)
-    results.power_spectra_from_transfer(inflation_params)
-    return results.get_tensor_cls(lmax)[:, 2] * 7.42835025e12
+  inflation_params = initialpower.InitialPowerParams()
+  if nt is None:
+    nt = -r / 8.
+  inflation_params.set_params(As=2.1e-9, r=r, nt=nt)
+  results.power_spectra_from_transfer(inflation_params)
+  return results.get_tensor_cls(lmax)[:, 2] * 7.42835025e12
 
 
 clbb_tens.cache_clear()
@@ -527,16 +547,17 @@ plt_func(ell, ell * (ell + 1) * np.array(B_res3[-1]) /
 
 # plt_func(clbb(r=0.01, lmax=3000), label=r'$C^{BB^{\rm{tot}}}_{\ell}$')
 fact = np.arange(0, 4001) * (np.arange(0, 4001) + 1.) / 2. / np.pi
-plt.plot(np.arange(0, 4001), fact * nl(9, 1, 4000), color = 'black', linestyle='--',
+plt.plot(np.arange(0, 4001), fact * nl(9, 1, 4000), color='black', linestyle='--',
          linewidth=font_size / 14.5, alpha=0.7)
-plt.plot(np.arange(0, 4001), fact * nl(1, 1, 4000), color = 'black', linestyle='--',
+plt.plot(np.arange(0, 4001), fact * nl(1, 1, 4000), color='black', linestyle='--',
          linewidth=font_size / 14.5, alpha=0.7)
 plt.ylim(1e-5, 1.5e-1)
 plt.xlim(10, 1700)
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$\ell(\ell+1)C^{BB}_{\ell}/ 2 \pi [ \mu K^{2} ]$')
 plt.legend(loc=0)
-plt.text(100, 2.e-2, 'SPTPol Noise', rotation=40, va='bottom', ha='left', fontsize=font_size / 1.7)
+plt.text(100, 2.e-2, 'SPTPol Noise', rotation=40,
+         va='bottom', ha='left', fontsize=font_size / 1.7)
 plt.text(600, 2.8e-3, 'CMB S4 Noise', rotation=40,
          va='bottom', ha='left', fontsize=font_size / 1.7)
 
